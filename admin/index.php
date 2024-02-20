baeaced6c8050bfca2e960dc39047f083c7dde5e
Isaacuch <58433931+Isaacuch@users.noreply.github.com>
2024-02-19 23:11:15 -0800

    Isaac
    cambios

--- admin/index.php
+++ admin/index.php
@@ -32,7 +32,7 @@ if (!isset($_SESSION['user_login'])) {
   <body>
 
   
-<nav class="navbar navbar-expand-lg navbar-white grey bg-white grey">
+<nav class="list-group-item list-group-item-action active">
   <div id="logo-container ">
     <img id="Logo" src="./images/logo1.png" alt="Logo de la empresa">
   </div>
@@ -40,7 +40,6 @@ if (!isset($_SESSION['user_login'])) {
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="separador"></div> <!-- Línea de separación -->
   <div class="navbar-collapse collapse justify-content-end" id="navbarSupportedContent">
     <?php $showuser = $_SESSION['user_login']; $haha = mysqli_query($db_con,"SELECT * FROM `users` WHERE `username`='$showuser';"); $showrow=mysqli_fetch_array($haha); ?>
     <ul class="nav navbar-nav ">

d09da56a0f08691a7773d9c5fcd467b44e11e7ff
Isaacuch <58433931+Isaacuch@users.noreply.github.com>
2024-02-20 16:48:13 -0800

    Isaac
    Colores aptos

--- admin/all-student.php
+++ admin/all-student.php
@@ -51,7 +51,7 @@
   </div>
     <?php } ?>
 <table class="table  table-striped table-hover table-bordered" id="data">
-  <thead class="thead-dark">
+  <thead class="thead-soft light red">
     <tr>
       <th scope="col">SL</th>
       <th scope="col">Nombre</th>

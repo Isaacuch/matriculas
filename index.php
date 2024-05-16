eee32e3b1899edf4b814d3eb1e3ee47e93a1b141
Isaacuch <58433931+Isaacuch@users.noreply.github.com>
2024-05-16 08:18:43 -0700

    isaac
    prueba de formatos

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ con su correpondiente validación de cada campo solicitado
             </div>
             <div class="form-group">
               <label for="roll">Número de Cédula</label>
-              <input name="roll" type="text" value="<?= isset($roll) ? $roll : ''; ?>" class="form-control" pattern="[0-9]{9}" id="roll" required="">
+              <input name="roll" type="text" value="<?= isset($roll) ? $roll : ''; ?>" class="form-control" pattern="[0-9]{9}" id="roll" required="Formato no establecido">
             </div>
 
             <div class="form-group">
